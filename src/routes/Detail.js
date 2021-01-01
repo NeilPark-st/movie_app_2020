9ced8aceac7204f353ddfb25f4f0793cf5124f02
Neil Park <yp73@waikato.ac.nz>
2021-01-01 21:41:55 +0900

    class components to function components

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-class Detail extends React.Component{
-    componentDidMount() {
-        const { location, history } = this.props;
+function Detail({ location, history }){
+    useEffect(() => {
         if(location.state === undefined){
             history.push('/');
         }
-    }
-
-    render() {
-        const { location } = this.props;
-        if(location.state){
-            const { title, year, poster, genres, summary} = location.state;
-            return (
-                <div style={{textAlign:"center"}}>
-                    <h1>{title}</h1>
-                    <h4>published in {year} </h4>
+    }, []);
+    if(location.state){
+        const { title, year, poster, genres, summary} = location.state;
+        return (
+            <div style={{textAlign:"center"}}>
+                <h1>{title}</h1>
+                <h4>published in {year} </h4>
+                <p>
                     {
                         genres.map((genre, index) => <span key={index} style={{marginRight:"5px"}}>{genre}</span>)
                     }
-                    <img src={poster} alt={title} title={title} />
-                    <br />
-                    <div style={{justifyContent:"center", alignItems:"center", display:"flex"}}>
-                        <span style={{width:"70%"}}>{summary}</span>
-                    </div>
+                </p>
+                <img src={poster} alt={title} title={title} />
+                <br />
+                <div style={{justifyContent:"center", alignItems:"center", display:"flex"}}>
+                    <span style={{width:"70%"}}>{summary}</span>
                 </div>
-
-           );
-        } else {
-            return null;
-        }
+            </div>
+        );
+    } else{
+        return null;
     }
 }
+
+// class Detail extends React.Component{
+//     componentDidMount() {
+//         const { location, history } = this.props;
+//         if(location.state === undefined){
+//             history.push('/');
+//         }
+//     }
+
+//     render() {
+//         const { location } = this.props;
+//         if(location.state){
+//             const { title, year, poster, genres, summary} = location.state;
+//             return (
+//                 <div style={{textAlign:"center"}}>
+//                     <h1>{title}</h1>
+//                     <h4>published in {year} </h4>
+//                     {
+//                         genres.map((genre, index) => <span key={index} style={{marginRight:"5px"}}>{genre}</span>)
+//                     }
+//                     <img src={poster} alt={title} title={title} />
+//                     <br />
+//                     <div style={{justifyContent:"center", alignItems:"center", display:"flex"}}>
+//                         <span style={{width:"70%"}}>{summary}</span>
+//                     </div>
+//                 </div>
+
+//            );
+//         } else {
+//             return null;
+//         }
+//     }
+// }
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,55 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Movie from '../components/Movie';
 import './Home.css'
 
-class Home extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  getMovies = async () => {
+  const getMovies = async () => {
     const {
       data: {
         data: { movies },
       },
     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false });
-  }
+    setMovies(movies);
+    setIsLoading(false);
+  };
 
-  componentDidMount(){
-    // data loading for movie data
-    this.getMovies();
-  }
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  render(){
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span> 
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader_text">Loading...</span> 
+        </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
           </div>
-          ) : (
-            <div className="movies">
-              {movies.map((movie) => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )}
-      </section>
-    );
-  }
+        )}
+    </section>
+  );
 }
+
+// class Home extends React.Component {
+//   state = {
+//     isLoading: true,
+//     movies: [],
+//   };
+
+//   getMovies = async () => {
+//     const {
+//       data: {
+//         data: { movies },
+//       },
+//     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+//     this.setState({ movies, isLoading: false });
+//   }
+
+//   componentDidMount(){
+//     // data loading for movie data
+//     this.getMovies();
+//   }
+
+//   render(){
+//     const { isLoading, movies } = this.state;
+//     return (
+//       <section className="container">
+//         {isLoading ? (
+//           <div className="loader">
+//             <span className="loader_text">Loading...</span> 
+//           </div>
+//           ) : (
+//             <div className="movies">
+//               {movies.map((movie) => (
+//                 <Movie
+//                   key={movie.id}
+//                   id={movie.id}
+//                   year={movie.year}
+//                   title={movie.title}
+//                   summary={movie.summary}
+//                   poster={movie.medium_cover_image}
+//                   genres={movie.genres}
+//                 />
+//               ))}
+//             </div>
+//           )}
+//       </section>
+//     );
+//   }
+// }
 
 export default Home;
